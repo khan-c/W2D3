3f05541e2d44d58630895509e8078cc5d14ed16d
Kyle Chen <kyle.h.chen@gmail.com>
2017-10-11 15:21:41 -0700

    Complete Towers of Hanoi TDD exercise

--- test_driven_development/lib/towers_of_hanoi.rb
+++ test_driven_development/lib/towers_of_hanoi.rb
@@ -1,3 +1,38 @@
 class TowersOfHanoi
+  attr_accessor :tower1, :tower2, :tower3, :towers
 
+  def initialize(size = 4)
+    @tower1 = (1..size).map { |num| num }
+    @tower2 = []
+    @tower3 = []
+    @towers = [@tower1, @tower2, @tower3]
+  end
+
+  def move(start, finish)
+    start_tower = @towers[start - 1]
+    end_tower = @towers[finish - 1]
+
+    if start_tower.empty?
+      raise 'Tower empty!'
+    elsif end_tower.empty?
+      end_tower.unshift(start_tower.shift)
+    elsif start_tower.first > end_tower.first
+      raise 'Cannot do this!'
+    else
+      end_tower.unshift(start_tower.shift)
+    end
+  end
+
+  def prompt
+    puts 'Please select a starting tower as a number.'
+    start_tower = gets.chomp.to_i
+    puts 'Please select an ending tower as a number.'
+    end_tower = gets.chomp.to_i
+
+    move(start_tower, end_tower)
+  end
+
+  def won?
+    @tower1.empty? && (@tower2.empty? || @tower3.empty?)
+  end
 end
--- test_driven_development/spec/towers_of_hanoi_spec.rb
+++ test_driven_development/spec/towers_of_hanoi_spec.rb
@@ -7,14 +7,14 @@ describe TowersOfHanoi do
   describe '#initialize' do
     context 'when no argument passed into initialize' do
       it 'initializes tower1 with four discs' do
-        expects(towers.tower1).to eq([1, 2, 3, 4])
+        expect(towers.tower1).to eq([1, 2, 3, 4])
       end
     end
 
     context 'when an argument is passed into initialize' do
       another_tower = TowersOfHanoi.new(7)
       it 'initializes tower1 with passed argument' do
-        expects(another_tower.tower1).to eq([1, 2, 3, 4, 5, 6, 7])
+        expect(another_tower.tower1).to eq([1, 2, 3, 4, 5, 6, 7])
       end
     end
 
@@ -22,16 +22,50 @@ describe TowersOfHanoi do
       expect(towers.tower2).to be_empty
       expect(towers.tower3).to be_empty
     end
+
+    it 'initializes a towers array containing the three towers' do
+      expect(towers.towers).to eq([towers.tower1, towers.tower2, towers.tower3])
+    end
   end
 
   describe '#move' do
-    it 'removes topmost disk from starting tower' do
+    before(:each) do
+      towers.move(1, 2)
+    end
 
+    it 'removes topmost disk from starting tower' do
+      expect(towers.tower1).to eq([2, 3, 4])
     end
 
     it 'adds disk to the top of ending tower' do
-      
+      expect(towers.tower2).to eq([1])
+    end
+
+    context 'when the receiving tower is not empty' do
+      it 'accepts disk if disk to be added is less than topmost disk' do
+        another_tower = TowersOfHanoi.new
+        another_tower.move(1, 2)
+        another_tower.move(1, 3)
+        expect { another_tower.move(2, 3) }.not_to raise_error
+      end
+
+      it 'rejects disk if disk to be added is greater than topmost disk' do
+        another_tower = TowersOfHanoi.new
+        another_tower.move(1, 2)
+        expect { another_tower.move(1, 2) }.to raise_error('Cannot do this!')
+      end
     end
   end
 
+  describe '#won?' do
+    it 'returns true if tower2 or tower3 contains all disks' do
+      towers.tower1 = []
+      towers.tower2 = [1, 2, 3, 4]
+      expect(towers.won?).to be true
+
+      towers.tower2 = []
+      towers.tower3 = [1, 2, 3, 4]
+      expect(towers.won?).to be true
+    end
+  end
 end
